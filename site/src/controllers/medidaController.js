b4518d5b855a1bda4a5c9b35d94861bf49ae3c40
Burlina <nathalia.marques@sptech.school>
2022-06-08 14:41:08 -0300

    atualização grafico

--- site/src/controllers/medidaController.js
+++ site/src/controllers/medidaController.js
@@ -2,7 +2,7 @@ var medidaModel = require("../models/medidaModel");
 
 function buscarUltimasMedidas(req, res) {
 
-    const limite_linhas = 7;
+    const limite_linhas = 15;
 
     var idTransporte = req.params.idTransporte;
 
